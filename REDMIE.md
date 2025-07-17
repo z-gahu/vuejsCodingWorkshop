73c7c2acea54ede5dab5a8d338190314fc1e2c7c
데스크탑 <z_gahu@naver.com>
2025-07-18 08:16:06 +0900

    chapt2

---
+++ REDMIE.md
@@ -0,0 +1,4 @@
+책소스
+https://github.com/gilbutITbook/007024
+
+Vue.js 2.6.11 사용
---
+++ product-data.js
@@ -0,0 +1,12 @@
+const data = {
+  sitename: "Vue.js 애완용품샾",
+  product: {
+    id: 1001,
+    title: "고양이 사료 - 1kg",
+    descript:
+      "고양이 사료는 영양이 풍부하고 맛있는 고양이 전용 사료입니다. 1kg 용량으로 제공됩니다.",
+    price: 15000,
+    image:
+      "https://github.com/gilbutITbook/007024/blob/master/chapter-02/assets/images/product-fullsize.png",
+  },
+};

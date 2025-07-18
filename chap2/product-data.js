08e75ccca07716edc95bd401f77fb4e5c9f9805d
데스크탑 <z_gahu@naver.com>
2025-07-18 17:12:09 +0900

    chapt연습

--- chap2/product-data.js
+++ chap2/product-data.js
@@ -7,6 +7,7 @@ const data = {
       "고양이 사료는 영양이 풍부하고 맛있는 고양이 전용 사료입니다. 1kg 용량으로 제공됩니다.",
     price: 15000,
     image:
-      "https://github.com/gilbutITbook/007024/blob/master/chapter-02/assets/images/product-fullsize.png",
+      // "https://github.com/gilbutITbook/007024/blob/master/chapter-02/assets/images/product-fullsize.png",
+      "/assets/images/product-fullsize.png",
   },
 };
